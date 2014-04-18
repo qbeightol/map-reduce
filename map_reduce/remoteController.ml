7b28ea3f5579d1573a3a73d3a480cb70e2e03f43
qeb2 <qeb2@cornell.edu>
2014-04-18 22:07:41 +0000

    fixed remoteController and worker

--- map_reduce/remoteController.ml
+++ map_reduce/remoteController.ml
@@ -4,25 +4,22 @@ exception InfrastructureFailure
 exception MapFailure of string 
 exception ReduceFailure of string 
 
-let addr_q = AQueue.create()
-let valid_addrs = ref []
+let worker_q = AQueue.create()
+let starting_addrs = ref []
+let valid_workers = ref []
 
 let init addrs = 
-    let store a = valid_addrs :=  a::(!valid_addrs) in 
+    let store a = starting_addrs :=  a::(!starting_addrs) in 
         List.iter (store) addrs
 
 (*removes an address from the list of valid addresses; if this list becomes 
 empty--which would imply that the remoteController has run out of workers, this
 function will raise an InfrastructureFailure exception*)
-let remove_bad_addr addr =
-    valid_addrs := List.filter ((<>) addr) (!valid_addrs);
-    match (!valid_addrs) with
+let remove_bad_addr (r,w) =
+    valid_workers := List.filter ((<>) (r,w)) (!valid_workers);
+    match (!valid_workers) with
     | [] -> raise InfrastructureFailure
     | _ -> ()
-(*I might need to close the connection with the bad_addr too*)
-
-let to_def_list lst =
-    Deferred.List.init (List.length lst) (fun x -> return(List.nth lst x))
 
 module Make (Job : MapReduce.Job) = struct
 
@@ -30,78 +27,113 @@ module Make (Job : MapReduce.Job) = struct
     module WReq = Protocol.WorkerRequest (Job)
     module WResp = Protocol.WorkerResponse (Job)
 
-    (*Executes a function on (s,r,w), a socket, reader, writer tuple using one
-    of the workers accesible via the address queue*)
-    let execute f =
-        AQueue.pop addr_q >>= 
-        begin fun (h,p) -> 
-            try_with (fun () -> Tcp.connect (Tcp.to_host_and_port h p) >>| f )
-            >>| function
-            | Core.Std.Result.Error _ -> remove_bad_addr (h,p); None
-            | Core.Std.Result.Ok res -> AQueue.push addr_q (h,p); Some (res)
-        end
-
     let start_connections () : unit Deferred.t =
-        let f (s,r,w) = Writer.write_line w Job.name in
+        let f (s,r,w) = print_endline "writing job name"; Writer.write_line w (Job.name); print_endline (Job.name^"was written"); (r,w) in
         let connect (h,p) = 
             try_with (fun () -> Tcp.connect (Tcp.to_host_and_port h p) >>| f )
             >>| function
-            | Core.Std.Result.Error _ -> remove_bad_addr (h,p)
-            | Core.Std.Result.Ok _ -> AQueue.push addr_q (h,p)
+            | Core.Std.Result.Error _ -> print_endline "bad connection"
+            | Core.Std.Result.Ok (r,w) -> print_endline ("good connection: "^h^" "^string_of_int p); AQueue.push worker_q (r,w); valid_workers :=  (r,w)::(!valid_workers)
         in 
-        (to_def_list (!valid_addrs))
+        return (!starting_addrs)
         >>= (fun addrs -> Deferred.List.iter addrs (connect))
 
-            (*This seems unsafe; I should see if there's a better approach*)
-            (*blah, at least it builds the addr_q*)
+    (*Executes a function on (s,r,w), a socket, reader, writer tuple using one
+    of the workers accesible via the worker queue. Returns the result of f along
+    with the reader, writer pair that executed it*)
+    let execute f =
+        print_endline "queuing workers";
+        AQueue.pop worker_q >>=
+        begin fun (r,w) ->
+            try_with (fun () -> f (r,w))
+            >>| function
+            | Core.Std.Result.Error e -> print_endline "error (not JobFailed) during execute"; remove_bad_addr (r,w); (None, (r,w))
+            | Core.Std.Result.Ok res -> print_endline "work completeled succesfully; verifying results worker added back to the queue"; AQueue.push worker_q (r,w); (Some res, (r,w))
+        end
 
-    let rec process_input (input : Job.input) : (Job.key * Job.inter) list Deferred.t  =
-        let f (s,r,w) = 
+    let rec process_input (input: Job.input) : (Job.key * Job.inter) list Deferred.t =
+        let f (r,w) : WResp.t Deferred.t =
+            print_endline "sending out a map request";
             WReq.send w (WReq.MapRequest input);
+            print_endline "waiting for a map response";
             WResp.receive r
-            >>= fun resp -> 
-                match resp with
-                | `Eof -> failwith "connection was closed unexpectedly" 
-                | `Ok resp ->
-                    begin 
-                        match resp with
-                        | WResp.JobFailed s -> raise (MapFailure s)
-                        | WResp.ReduceResult _ -> failwith "wrong response"
-                        | WResp.MapResult l -> (return l)
-                    end 
-        in execute f 
-        >>= (fun res ->
-            match res with
-            | None -> process_input input
-            | Some x -> x)
-        (*fix so that it gets the requests from the workers*)
+            >>| begin fun resp ->
+                begin
+                    match resp with
+                    | `Eof -> failwith "connection was closed unexpectedly" 
+                    | `Ok resp -> 
+                        begin
+                            print_endline "received a map response";
+                            resp
+                        end
+                end
+            end 
+        in execute f
+        >>= begin fun (resp_opt, (r,w)) ->
+            print_endline "returning a map result";
+            match resp_opt with
+            | None -> print_endline "map failed; re-requesting"; process_input input
+            | Some x ->
+                begin
+                    match x with
+                    | WResp.JobFailed s -> raise (MapFailure s)
+                    | WResp.ReduceResult _ -> 
+                        begin
+                            print_endline "worker provided the wrong response; removing it from the queue and re-requesting";
+                            remove_bad_addr (r,w);
+                            process_input input
+                        end 
+                    | WResp.MapResult l -> print_endline "valid map response"; return l
+                end
+            end
 
     let rec process_intermediate ((k : Job.key), (ilst: Job.inter list)) : Job.output Deferred.t  =
-        let f (s,r,w) = 
+        let f (r,w) : WResp.t Deferred.t =
+            print_endline "sending out a reduce request";
             WReq.send w (WReq.ReduceRequest (k, ilst));
+            print_endline "waiting for a reduce response";
             WResp.receive r
-            >>= fun resp -> 
-                match resp with
-                | `Eof -> failwith "connection was closed unexpectedly" 
-                | `Ok resp ->
-                    begin 
-                        match resp with
-                        | WResp.JobFailed s -> raise (ReduceFailure s)
-                        | WResp.MapResult _ -> failwith "wrong response"
-                        | WResp.ReduceResult o -> return o
-                    end 
+            >>| begin fun resp ->
+                begin
+                    match resp with
+                    | `Eof -> failwith "connection was closed unexpectedly" 
+                    | `Ok resp -> 
+                        begin
+                            print_endline "received a reduce response";
+                            resp
+                        end
+                end
+            end 
         in execute f
-        >>= (fun res ->
-            match res with
-            | None -> process_intermediate (k, ilst)
-            | Some x -> x)    
+        >>= begin fun (resp_opt, (r,w)) ->
+            print_endline "returning a reduce result";
+            match resp_opt with
+            | None -> print_endline "reduce failed; re-requesting"; process_intermediate (k, ilst)
+            | Some x ->
+                begin
+                    match x with
+                    | WResp.JobFailed s -> raise (ReduceFailure s)
+                    | WResp.MapResult _ -> 
+                        begin
+                            print_endline "worker provided the wrong response; removing it from the queue and re-requesting";
+                            remove_bad_addr (r,w);
+                            process_intermediate (k, ilst)
+                        end 
+                    | WResp.ReduceResult l -> print_endline "valid map response"; return l
+                end  
+        end 
 
     (*val map_reduce : Job.input list -> (Job.key * Job.output) list Deferred.t*)
     let map_reduce inputs =
-        start_connections ()
-        >>= (fun () -> Warmup.deferred_map inputs (process_input))
-        >>| (fun lst -> Comb.combine (List.flatten lst))
-        >>= (fun lst -> Warmup.deferred_map lst (fun (k, ilst) -> (process_intermediate (k, ilst)) >>= (fun x -> return (k, x)) ))
+
+        (start_connections ())
+        >>= (fun () ->
+            print_endline "connections started";
+            if (!valid_workers) == [] then raise InfrastructureFailure
+            else (return inputs)
+            >>= (fun lst -> print_endline "mapping"; Deferred.List.map ~how: `Parallel ~f: (process_input) lst)  
+            >>| (fun lst ->  print_endline "combining"; Comb.combine (List.flatten lst))
+            >>= (fun lst -> print_endline "reducing"; Deferred.List.map ~how: `Parallel ~f: (fun (k, ilst) -> (process_intermediate (k, ilst)) >>= (fun x -> return (k, x))) lst))
 
 end
 
--- map_reduce/worker.ml
+++ map_reduce/worker.ml
@@ -4,45 +4,64 @@ module Make (Job : MapReduce.Job) = struct
 
   (* see .mli *)
   let run r w =
-  (*At some point, we'll need to use mapReduce.get_job on the job name sent to
-  the worker. Then we can make modules out of the WorkerRequest and 
-  WorkerResponse functors.*)
+    print_endline "I'm doing something!";
+    
 
-  (*Get the job name from the remote controller*)
-  Reader.read_line r 
-  >>= function
-  | `Eof -> return ()
-  | `Ok name -> 
-    match (MapReduce.get_job name) with
-    | None -> return ()
-    | Some (module J : MapReduce.Job) ->
-      begin
-        (*Do i need ins here?*)
-        
-        let module WReq = Protocol.WorkerRequest (J) in
-        let module WResp = Protocol.WorkerResponse (J) in
+    let module WReq = Protocol.WorkerRequest (Job) in
+    let module WResp = Protocol.WorkerResponse (Job) in
 
-        WReq.receive r >>= function
-        | `Eof -> return ()
-        | `Ok m -> 
-          begin
-            match m with 
-            | WReq.MapRequest j -> 
-              begin
-                J.map j >>= function
-                | [] -> return ()
-                | kvp -> return (WResp.send w (WResp.MapResult kvp))
-              end
-            | WReq.ReduceRequest (k, is) -> 
-              begin
-                J.reduce (k,is) >>= function
-                | out -> return (WResp.send w (WResp.ReduceResult out))
-              end
+    let rec process_requests () : unit Deferred.t =
+      print_endline "waiting for message";
+      WReq.receive r >>= function
+      | `Eof -> print_endline "end of file"; return ()
+      | `Ok m ->
+        print_endline "received message"; 
+        begin
+          match m with 
+          | WReq.MapRequest j -> 
+            print_endline "got MapRequest";
+            begin
+              print_endline "processing MapRequest";
+              (Job.map j) >>= function
+              | [] -> print_endline "MapRequest contained an empty list"; return ()
+              | kvp -> process_requests (WResp.send w (WResp.MapResult kvp))
+            end
+          | WReq.ReduceRequest (k, is) -> 
+            print_endline "got ReduceRequest";
+            begin
+              print_endline "processing ReduceRequest";
+              Job.reduce (k,is) >>= function
+              | out -> process_requests (WResp.send w (WResp.ReduceResult out))
+            end
           end
-      end 
+    in process_requests ()
+
 end
 
 (* see .mli *)
+let init port =
+  Tcp.Server.create
+    ~on_handler_error:`Raise
+    (Tcp.on_port port)
+    (fun _ r w ->
+      Reader.read_line r >>= function
+        | `Eof    -> return ()
+        | `Ok job -> print_endline ("JOB: "^job); match MapReduce.get_job job with
+          | None -> print_endline "wah?"; return ()
+          | Some j ->
+            print_endline ("starting to run " ^job);
+            let module Job = (val j) in
+            let module Worker = Make(Job) in
+            Worker.run r w
+    )
+    >>= fun _ ->
+  print_endline "server started";
+  print_endline "worker started.";
+  print_endline "registered jobs:";
+  List.iter print_endline (MapReduce.list_jobs ());
+  never ()
+
+
 let init port =
   Tcp.Server.create
     ~on_handler_error:`Raise
@@ -63,5 +82,4 @@ let init port =
   print_endline "registered jobs:";
   List.iter print_endline (MapReduce.list_jobs ());
   never ()
-
 
