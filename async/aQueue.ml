42e2af2058dfd37178b282927b828f9c4abcd10d
Quinn Beightol <qeb2@cornell.edu>
2014-09-24 08:43:53 -0400

    revised aQueue.ml for readability

--- async/aQueue.ml
+++ async/aQueue.ml
@@ -5,21 +5,18 @@ exception Closed_queue (*shouldn't be thrown--I haven't included any code that
                   closes pipes--but it's here in case a pipe does get closed*)
 
 (* see .mli *)
-let create () =
-    Pipe.create()
+let create () = Pipe.create()
 
 (* see .mli *)
-let push (q: 'a t) x =
-    match q with (_, w) -> 
-        if not (Pipe.is_closed w) then Pipe.write_without_pushback w x 
-        else raise Closed_queue
+let push (_,w) x = 
+  if Pipe.is_closed w then raise Closed_queue
+  else Pipe.write_without_pushback w x 
 
 (* see .mli *)
-let pop  (q: 'a t) =
-    match q with (r, _) -> (Pipe.read r)
-        >>= function
-            | `Eof -> raise Closed_queue
-            | `Ok x -> return x
+let pop  (r,_) = 
+  Pipe.read r >>= function
+                  | `Eof -> raise Closed_queue
+                  | `Ok x -> return x
 
 
 
