8ce8ae9e1f7126d11c04868f6d6a58a93b7414f3
Quinn Beightol <qeb2@cornell.edu>
2014-09-24 13:54:51 -0400

    cleaned up warmup solutions

--- async/warmup.ml
+++ async/warmup.ml
@@ -1,14 +1,16 @@
 
 open Async.Std
 
-let fork d f1 f2 = ignore (Deferred.both (d >>= f1) (d>>=f2))
+let fork d f1 f2 = ignore (d >>= f1); ignore (d>>= f2)
 
-let deferred_map l  (f: 'a -> 'b Deferred.t) =
-    let f' e acc=
-        acc
-        >>= fun acc -> (f e)
-        >>= fun mapped_e -> return (mapped_e::acc)
-    in List.fold_right f' l (return []);;
+let my_both dx1 dx2 = dx1 >>= (fun x1 -> dx2 >>= fun x2 -> return (x1, x2))
+
+let my_all (l: 'a Deferred.t list) : 'a list Deferred.t = 
+  let f de dacc = my_both de dacc >>= (fun (e,acc) -> return (e::acc))
+  in List.fold_right f l (return []) 
+
+let deferred_map l f = my_all (List.map f l)
+
 
 
 
