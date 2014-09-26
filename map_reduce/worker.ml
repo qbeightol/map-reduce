dbfd40b4e1c997807b6403c0bc3f48d9a6e711f8
Quinn Beightol <qeb2@cornell.edu>
2014-09-26 12:45:12 -0400

    added pdf overviews and revised worker.ml

--- map_reduce/worker.ml
+++ map_reduce/worker.ml
@@ -1,57 +1,32 @@
 open Async.Std
 
 module Make (Job : MapReduce.Job) = struct
+  
+  module WReq  = Protocol.WorkerRequest(Job)
+  module WResp = Protocol.WorkerRequest(Job) 
+
+  let rec process_requests () = unit Deferred.t = 
+    WReq.receive r 
+    >>= function 
+    | `Eof -> return ()
+    | `Ok (WReq.MapRequest j) -> 
+        Job.map j 
+        >>= fun result -> 
+          (*once the worker has finished the job, send out the resulting list
+            of (key, intermediate value) pairs and process the next request*)
+          WResp.send w (WResp.mapResult result);
+          process_requests ()
+    | `Ok (WReq.ReduceRequest req) -> 
+        Job.reduce req
+        >>= fun out -> 
+          WResp.send w (WResp.ReduceResult out);
+          process_requests ()
 
   (* see .mli *)
-  let run r w =
-    let module WReq = Protocol.WorkerRequest (Job) in
-    let module WResp = Protocol.WorkerResponse (Job) in
-
-    let rec process_requests () : unit Deferred.t =
-      WReq.receive r >>= function
-      | `Eof -> return ()
-      | `Ok m ->
-        begin
-          match m with 
-          | WReq.MapRequest j -> 
-            begin
-              (Job.map j) >>= function
-              | kvp -> process_requests (WResp.send w (WResp.MapResult kvp))
-            end
-          | WReq.ReduceRequest (k, is) -> 
-            begin
-              Job.reduce (k,is) >>= function
-              | out -> process_requests (WResp.send w (WResp.ReduceResult out))
-            end
-          end
-    in process_requests ()
-
+  let run r w = process_requests ()
 end
 
 (* see .mli *)
-let init port =
-  Tcp.Server.create
-    ~on_handler_error:`Raise
-    (Tcp.on_port port)
-    (fun _ r w ->
-      Reader.read_line r >>= function
-        | `Eof    -> return ()
-        | `Ok job -> print_endline ("JOB: "^job); match MapReduce.get_job job with
-          | None -> print_endline "wah?"; return ()
-          | Some j ->
-            print_endline ("starting to run " ^job);
-            let module Job = (val j) in
-            let module Worker = Make(Job) in
-            Worker.run r w
-    )
-    >>= fun _ ->
-  print_endline "server started";
-  print_endline "worker started.";
-  print_endline "registered jobs:";
-  List.iter print_endline (MapReduce.list_jobs ());
-  never ()
-
-
 let init port =
   Tcp.Server.create
     ~on_handler_error:`Raise
