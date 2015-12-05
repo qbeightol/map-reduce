f5b3f38437b07dd22ec07c903bd8a495d52b0c60
Quinn Beightol <qeb2@cornell.edu>
2015-12-05 15:42:35 -0500

    style tweak

--- async/warmup.ml
+++ async/warmup.ml
@@ -15,7 +15,9 @@ let my_both dx1 dx2 = dx1 >>= (fun x1 -> dx2 >>= fun x2 -> return (x1, x2))
 
 (*similar to all in Std.Deferred*)
 let my_all (l: 'a Deferred.t list) : 'a list Deferred.t = 
-  let f de dacc = my_both de dacc >>= (fun (e,acc) -> return (e::acc))
+  let f de dacc = 
+    my_both de dacc >>= fun (e,acc) -> 
+    return (e::acc)
   in List.fold_right f l (return []) 
 
 (*like the List module map, but allows the processing of each element to occur
